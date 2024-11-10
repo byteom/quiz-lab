16972ea606156fb5d0183fa2fb65ee1bc701acf0
byteom <123omsinghpipariya@gmail.com>
2024-11-11 00:09:55 +0530

    co

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteExternalsPlugin from 'vite-plugin-externals';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['some-external-module'], // Replace 'some-external-module' with the actual module name
-    },
-  },
+  plugins: [
+    react(),
+    viteExternalsPlugin({
+      'some-external-module': 'SomeGlobalVariable', // replace with actual module/global variable
+    }),
+  ],
 });
